2466f6b821e9181d8eb7d9989a8d56dcc2d276f9
Daniel-Ferreira-2004 <danikferreira69@gmail.com>
2025-12-19 17:44:43 -0300

    Add user registration and confirm password field
    Implemented user registration logic in AccountController using UserManager and SignInManager. Updated ChangePassword view to include a ConfirmNewPassword field for password confirmation.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LoginAPI.Models;
+using LoginAPI.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LoginAPI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<Users> _userManager;
+        private readonly SignInManager<Users> _signInManager;
+
+        public AccountController(UserManager<Users> userManager, SignInManager<Users> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         public IActionResult Login()
         {
             return View();
@@ -12,6 +23,35 @@ namespace LoginAPI.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Users users = new Users
+                {
+                    FullName = model.Name,
+                    Email = model.Email,
+                    UserName = model.Email,
+                };
+                var result = await _userManager.CreateAsync(users, model.Password);
+                if (result.Succeeded)
+                {
+                    // Additional logic such as sending a confirmation email can be added here
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
         public IActionResult VerifyEmail()
         {
             return View();
